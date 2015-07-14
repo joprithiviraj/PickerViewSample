80d66c397a20c1227167ccf1ecd52a1f19dd7460
prithiviraj <prithiviraj@Thouseefs-iMac.local>
2015-07-14 12:13:35 +0530

    Created the Xcode project.

---
+++ PickerViewSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PickerViewSample
+//
+//  Created by Prithiviraj on 7/14/15.
+//  Copyright (c) 2015 Prithiviraj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
